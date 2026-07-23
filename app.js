f02612e7affb03ddab047c606fbc68fe810edfe3
Rajdeep <rajdeep7488@gmail.com>
2026-07-23 20:40:01 +0530

    Improve error handling and error page rendering

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ app.all("/{*any}", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode=500, message="Something went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error.ejs",{ message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
