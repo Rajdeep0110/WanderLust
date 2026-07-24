79415e34831eca0e4c85064a77adb5048ead807f
Rajdeep <rajdeep7488@gmail.com>
2026-07-24 21:32:29 +0530

    Implement server-side validation using Joi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -55,8 +56,13 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 app.post(
   "/listings",
   wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-      throw new ExpressError(400, "Send valid data for listing");
+    // if(!req.body.listing){
+    //   throw new ExpressError(400, "Send valid data for listing");
+    // }
+    let result=listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+      throw new ExpressError(400, result.error)
     }
     console.log("=== FORM SUBMISSION DEBUG ===");
     console.log("Full req.body:", req.body);
@@ -70,6 +76,15 @@ app.post(
     console.log("==============================");
 
     const newListing = new Listing(req.body.listing);
+    //  if(!newListing.title){
+    //   throw new ExpressError(400, "Title is missing");
+    // }
+    // if(!newListing.description){
+    //   throw new ExpressError(400, "Description is missing");
+    // }
+    //  if(!newListing.location){
+    //   throw new ExpressError(400, "Location is missing");
+    // }
     await newListing.save();
 
     res.redirect("/listings");
